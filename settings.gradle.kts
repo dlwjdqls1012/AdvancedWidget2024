2ff7ddce7b8bc3631a87396f6df0fd8471eb58de
dlwjdqls1012 <dlwjdqls1356@gmail.com>
2024-04-18 15:01:24 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AdvancedWidget2024"
+include(":app")
